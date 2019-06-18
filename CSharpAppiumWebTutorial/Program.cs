4f5fbbb29df04dc2a43921c64f7a40641b880787
Matt Jasaitis <mattjasaitis@Matts-MacBook-Pro.local>
2019-06-18 13:31:53 -0700

    setup

---
+++ CSharpAppiumWebTutorial/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Applitools.Appium;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+
+namespace CSharpAppiumWebTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //iOS Test
+            Eyes eyes = new Eyes();
+            eyes.ApiKey = Environment.GetEnvironmentVariable("APPLITOOLS_API_KEY");
+
+            DesiredCapabilities dc = new DesiredCapabilities();
+            dc.SetCapability("platformName", "iOS");
+            dc.SetCapability("browserName", "Safari");
+            dc.SetCapability("deviceName", "iPhone XR");
+            dc.SetCapability("automationName", "XCUITest");
+            dc.SetCapability("platformVersion", "12.1");
+
+            RemoteWebDriver driver = new RemoteWebDriver(new Uri("http://localhost:4723/wd/hub"), dc);
+
+            //Error in tutorial on this line, should be below
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(60); 
+
+            try
+            {
+                eyes.Open(driver, "Hello World!", "C# Appium Web");
+                driver.Url = "https://applitools.com/helloworld";
+                eyes.CheckWindow("Hello!");
+                driver.FindElement(By.TagName("button")).Click();
+                eyes.CheckWindow("Click!");
+                eyes.Close(); 
+            }
+            finally
+            {
+                eyes.AbortIfNotClosed();
+                driver.Quit(); 
+            }
+        }
+    }
+}
